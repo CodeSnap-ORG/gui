68df9f08a15378e770423f760d8be78e5a85eadf
amp <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-17 15:30:31 +0000

    remove non-existent sound block from palette

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -308,12 +308,6 @@ const sound = function (isInitialSetup, isStage, targetId, soundName, colors) {
                     <field name="SOUND_MENU">${soundName}</field>
                 </shadow>
             </value>
-        </block><block id="${targetId}_sound_play_at_seconds" type="sound_play_at_seconds">
-            <value name="SOUND_MENU">
-                <shadow type="sound_sounds_menu">
-                    <field name="SOUND_MENU">${soundName}</field>
-                </shadow>
-            </value>
         </block>
         <block id="${targetId}_sound_play" type="sound_play">
             <value name="SOUND_MENU">
