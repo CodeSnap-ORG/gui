5912c08b363bfd6e231a3d2002660c4260723861
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-07 07:40:45 +0100

    Update render-interface.jsx

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -1,34 +1,32 @@
-import classNames from 'classnames'; 
-import PropTypes from 'prop-types'; 
-import React from 'react'; 
-import axios from 'axios'; 
-import { connect } from 'react-redux'; 
-import { compose } from 'redux'; 
-import { FormattedMessage, defineMessages, injectIntl, intlShape } from 'react-intl'; 
-import { getIsLoading } from '../reducers/project-state.js'; 
-import AppStateHOC from '../lib/app-state-hoc.jsx'; 
-import ErrorBoundaryHOC from '../lib/error-boundary-hoc.jsx'; 
-import TWProjectMetaFetcherHOC from '../lib/tw-project-meta-fetcher-hoc.jsx'; 
-import TWStateManagerHOC from '../lib/tw-state-manager-hoc.jsx'; 
-import SBFileUploaderHOC from '../lib/sb-file-uploader-hoc.jsx'; 
-import TWPackagerIntegrationHOC from '../lib/tw-packager-integration-hoc.jsx'; 
-import SettingsStore from '../addons/settings-store-singleton'; 
-import '../lib/tw-fix-history-api'; 
-import GUI from './render-gui.jsx'; 
-import MenuBar from '../components/menu-bar/menu-bar.jsx'; 
-import ProjectInput from '../components/tw-project-input/project-input.jsx'; 
-import FeaturedProjects from '../components/tw-featured-projects/featured-projects.jsx'; 
-import Description from '../components/tw-description/description.jsx'; 
-import BrowserModal from '../components/browser-modal/browser-modal.jsx'; 
-import CloudVariableBadge from '../containers/tw-cloud-variable-badge.jsx'; 
-import { isBrowserSupported } from '../lib/tw-environment-support-prober'; 
-import AddonChannels from '../addons/channels'; 
-import { loadServiceWorker } from './load-service-worker'; 
-import runAddons from '../addons/entry'; 
-import InvalidEmbed from '../components/tw-invalid-embed/invalid-embed.jsx'; 
-import { APP_NAME } from '../lib/brand.js'; 
-import Clippy from '../containers/amp-clippy.jsx'; 
-import Footer from '../components/amp-footer/footer.jsx'; 
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { FormattedMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
+import { getIsLoading } from '../reducers/project-state.js';
+import AppStateHOC from '../lib/app-state-hoc.jsx';
+import ErrorBoundaryHOC from '../lib/error-boundary-hoc.jsx';
+import TWProjectMetaFetcherHOC from '../lib/tw-project-meta-fetcher-hoc.jsx';
+import TWStateManagerHOC from '../lib/tw-state-manager-hoc.jsx';
+import SBFileUploaderHOC from '../lib/sb-file-uploader-hoc.jsx';
+import TWPackagerIntegrationHOC from '../lib/tw-packager-integration-hoc.jsx';
+import SettingsStore from '../addons/settings-store-singleton';
+import '../lib/tw-fix-history-api';
+import GUI from './render-gui.jsx';
+import MenuBar from '../components/menu-bar/menu-bar.jsx';
+import Description from '../components/tw-description/description.jsx';
+import BrowserModal from '../components/browser-modal/browser-modal.jsx';
+import CloudVariableBadge from '../containers/tw-cloud-variable-badge.jsx';
+import { isBrowserSupported } from '../lib/tw-environment-support-prober';
+import AddonChannels from '../addons/channels';
+import { loadServiceWorker } from './load-service-worker';
+import runAddons from '../addons/entry';
+import InvalidEmbed from '../components/tw-invalid-embed/invalid-embed.jsx';
+import { APP_NAME } from '../lib/brand.js';
+import Clippy from '../containers/amp-clippy.jsx';
+import Footer from '../components/amp-footer/footer.jsx';
 import styles from './interface.css';
 
 const isInvalidEmbed = window.parent !== window;
@@ -86,17 +84,12 @@ class Interface extends React.Component {
     }
 
     handleUpdateProjectTitle(title, isDefault) {
-        if (isDefault || !title) {
-            document.title = `${APP_NAME}`;
-        } else {
-            document.title = `${title} - ${APP_NAME}`;
-        }
+        document.title = isDefault || !title ? APP_NAME : `${title} - ${APP_NAME}`;
     }
 
     handleShareProject() {
-        const { vm, projectId } = this.props;
-        
-        const projectName = this.props.projectName;
+        const { vm, projectId, projectName, projectGenre } = this.props;
+
         const projectThumbnail = 'https://codesnap-org.github.io/projects/static/assets/018f79360b10f9f2c317d648d61a0eb2.svg';
         const projectLink = `https://codesnap-org.github.io/projects/?project_url=https://block-compiler-codesnap.onrender.com/projects/${projectId}`;
 
@@ -112,9 +105,8 @@ class Interface extends React.Component {
                     const formDataProject = new FormData();
                     formDataProject.append('name', projectName);
                     formDataProject.append('thumbnail', projectThumbnail);
-                    formDataProject.append('genre', this.props.projectGenre);
+                    formDataProject.append('genre', projectGenre);
                     formDataProject.append('link', projectLink);
-
                 })
                 .catch(err => {
                     console.error("Error sharing SB3 project:", err);
@@ -132,13 +124,13 @@ class Interface extends React.Component {
             const user = await this.authenticateUser(username, password);
             this.setState({ error: '' });
             alert(`Welcome back, ${user.username}`);
-        } catch (error) {
+        } catch {
             this.setState({ error: 'Login failed, creating a new account...' });
             try {
                 const newUser = await this.createNewAccount(username, password);
                 this.setState({ error: '' });
                 alert(`Welcome, new user ${newUser.username}`);
-            } catch (error) {
+            } catch {
                 this.setState({ error: 'Error creating new account.' });
             }
         }
@@ -146,28 +138,20 @@ class Interface extends React.Component {
 
     authenticateUser(username, password) {
         return new Promise((resolve, reject) => {
-            if (username === 'existingUser' && password === 'password123') {
-                resolve({ username });
-            } else {
-                reject('User not found');
-            }
+            username === 'existingUser' && password === 'password123'
+                ? resolve({ username })
+                : reject('User not found');
         });
     }
 
     createNewAccount(username, password) {
         return new Promise((resolve, reject) => {
-            if (username && password) {
-                resolve({ username });
-            } else {
-                reject('Invalid data');
-            }
+            username && password ? resolve({ username }) : reject('Invalid data');
         });
     }
 
     render() {
-        if (isInvalidEmbed) {
-            return <InvalidEmbed />;
-        }
+        if (isInvalidEmbed) return <InvalidEmbed />;
 
         const {
             intl,
@@ -183,17 +167,14 @@ class Interface extends React.Component {
 
         const isHomepage = isPlayerOnly && !isFullScreen;
         const isEditor = !isPlayerOnly;
-
-        // Check for 'project_url' in the URL parameters
         const urlParams = new URLSearchParams(window.location.search);
         const hasProjectUrlParam = urlParams.has('project_url');
 
         return (
-            <div
-                className={classNames(styles.container, {
-                    [styles.playerOnly]: isHomepage,
-                    [styles.editor]: isEditor
-                })}
+            <div className={classNames(styles.container, {
+                [styles.playerOnly]: isHomepage,
+                [styles.editor]: isEditor
+            })}
                 dir={isRtl ? 'rtl' : 'ltr'}
             >
                 <div className={styles.menu}>
@@ -204,28 +185,22 @@ class Interface extends React.Component {
                         enableSeeInside
                         onClickAddonSettings={handleClickAddonSettings}
                     />
-                    
-                    {/* Conditional rendering of the share button */}
                     {!hasProjectUrlParam && localStorage.getItem('username') && (
-                        <div className={styles.projectHeader}>
-                            <h2 className={styles.projectName}>{this.props.projectName}</h2>
-                            <button onClick={this.handleShareProject} className={styles.shareButton}>
-                                <FormattedMessage
-                                    defaultMessage="Share"
-                                    description="Share button"
-                                    id="tw.shareButton"
-                                />
-                            </button>
-                        </div>
+                        <button
+                            onClick={this.handleShareProject}
+                            className={styles.shareButton}
+                            style={{ position: 'absolute', top: '8px', right: '8px', zIndex: 10 }}
+                        >
+                            <FormattedMessage
+                                defaultMessage="Share"
+                                description="Share button"
+                                id="tw.shareButton"
+                            />
+                        </button>
                     )}
                 </div>
 
-                <div
-                    className={styles.center}
-                    style={isPlayerOnly ? ({
-                        width: `${Math.max(480, props.customStageSize.width) + 2}px`
-                    }) : null}
-                >
+                <div className={styles.center} style={isPlayerOnly ? { width: `${Math.max(480, props.customStageSize.width) + 2}px` } : null}>
                     <GUI
                         onClickAddonSettings={handleClickAddonSettings}
                         onUpdateProjectTitle={this.handleUpdateProjectTitle}
@@ -234,65 +209,32 @@ class Interface extends React.Component {
                         {...props}
                     />
 
-                    {isHomepage ? (
-                        <React.Fragment>
-                            {isBrowserSupported() ? (
-                                <Clippy isFixed messageSet="player" />
-                            ) : (
-                                <BrowserModal isRtl={isRtl} />
-                            )}
+                    {isHomepage && (
+                        <>
+                            {isBrowserSupported() ? <Clippy isFixed messageSet="player" /> : <BrowserModal isRtl={isRtl} />}
 
                             {(description.instructions === 'unshared' || description.credits === 'unshared') && (
                                 <div className={classNames(styles.infobox, styles.unsharedUpdate)}>
-                                    <p>
-                                        <FormattedMessage
-                                            defaultMessage="Unshared projects are no longer visible."
-                                            description="Appears on unshared projects"
-                                            id="tw.unshared2.1"
-                                        />
-                                    </p>
+                                    <p><FormattedMessage defaultMessage="Unshared projects are no longer visible." id="tw.unshared2.1" /></p>
                                     <p>
                                         <FormattedMessage
                                             defaultMessage="For more information, visit: {link}"
-                                            description="Appears on unshared projects"
                                             id="tw.unshared.2"
                                             values={{
-                                                link: (
-                                                    <a
-                                                        href="https://docs.turbowarp.org/unshared-projects"
-                                                        target="_blank"
-                                                        rel="noopener noreferrer"
-                                                    >
-                                                        https://docs.turbowarp.org/unshared-projects
-                                                    </a>
-                                                )
+                                                link: <a href="https://docs.turbowarp.org/unshared-projects" target="_blank" rel="noopener noreferrer">https://docs.turbowarp.org/unshared-projects</a>
                                             }}
                                         />
                                     </p>
-                                    <p>
-                                        <FormattedMessage
-                                            defaultMessage="If the project was shared recently, this message may appear incorrectly for a few minutes."
-                                            description="Appears on unshared projects"
-                                            id="tw.unshared.cache"
-                                        />
-                                    </p>
-                                    <p>
-                                        <FormattedMessage
-                                            defaultMessage="If this project is actually shared, please report a bug."
-                                            description="Appears on unshared projects"
-                                            id="tw.unshared.bug"
-                                        />
-                                    </p>
+                                    <p><FormattedMessage defaultMessage="If the project was shared recently, this message may appear incorrectly for a few minutes." id="tw.unshared.cache" /></p>
+                                    <p><FormattedMessage defaultMessage="If this project is actually shared, please report a bug." id="tw.unshared.bug" /></p>
                                 </div>
                             )}
 
                             {hasCloudVariables && projectId !== '0' && (
-                                <div className={styles.section}>
-                                    <CloudVariableBadge />
-                                </div>
+                                <div className={styles.section}><CloudVariableBadge /></div>
                             )}
 
-                            {description.instructions || description.credits ? (
+                            {(description.instructions || description.credits) && (
                                 <div className={styles.section}>
                                     <Description
                                         instructions={description.instructions}
@@ -300,13 +242,12 @@ class Interface extends React.Component {
                                         projectId={projectId}
                                     />
                                 </div>
-                            ) : null}
+                            )}
 
                             <div className={styles.section}>
                                 <p>
                                     <FormattedMessage
                                         defaultMessage="{APP_NAME} is a more powerful Scratch modification that lets you create complex projects easily. Try it out by clicking See Inside!"
-                                        description="Description of AmpMod on the homepage"
                                         id="tw.home.ampdescription"
                                         values={{ APP_NAME }}
                                     />
@@ -314,34 +255,19 @@ class Interface extends React.Component {
                             </div>
 
                             <div className={classNames(styles.infobox, styles.unsharedUpdate)}>
-                                <h3>
-                                    <FormattedMessage
-                                        defaultMessage="Heads up!"
-                                        description="Notice header"
-                                        id="tw.development.noticeHeader"
-                                    />
-                                </h3>
+                                <h3><FormattedMessage defaultMessage="Heads up!" id="tw.development.noticeHeader" /></h3>
                                 <p>
                                     <FormattedMessage
-                                        defaultMessage="CodeSnap is in its alpha stages. Please expect to notice radical changes. Compatibility with projects may be dropped in the near future. We may add a compatibility mode but do not get your hopes up. For updates, visit {link}."
-                                        description="Notice about active development"
+                                        defaultMessage="CodeSnap is in its alpha stages... For updates, visit {link}."
                                         id="tw.development.notice"
                                         values={{
-                                            link: (
-                                                <a
-                                                    href="https://scratch.mit.edu/discuss/topic/806311"
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                >
-                                                    https://scratch.mit.edu/discuss/topic/806311
-                                                </a>
-                                            )
+                                            link: <a href="https://scratch.mit.edu/discuss/topic/806311" target="_blank" rel="noopener noreferrer">scratch.mit.edu</a>
                                         }}
                                     />
                                 </p>
                             </div>
-                        </React.Fragment>
-                    ) : null}
+                        </>
+                    )}
                 </div>
 
                 {isHomepage && <Footer />}
