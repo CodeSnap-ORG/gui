ace3e6c4affe2b2fa2c84acb43b39c011e68ad61
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-10 08:18:22 +0100

    Update menu-bar.jsx

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -1016,14 +1016,14 @@ class MenuBar extends React.Component {
                     <div className={styles.menuBarItem}>
                         <a
                             className={styles.feedbackLink}
-                            href="https://scratch.mit.edu/discuss/topic/806311"
+                            href="https://codesnap-org.github.io/"
                             rel="noopener noreferrer"
                             target="_blank"
                         >
                             {/* todo: icon */}
                             <Button className={styles.feedbackButton}>
                                 <FormattedMessage
-                                    defaultMessage="AmpMod Forum Topic"
+                                    defaultMessage="CodeSnap Home"
                                     description="Button to give feedback in the menu bar"
                                     id="tw.topicButton"
                                     values={{
